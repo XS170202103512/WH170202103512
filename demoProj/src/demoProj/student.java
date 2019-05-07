3736554b1582f19f4bf73bb4e9628ec85cb4af9a
Administrator <Administrator@MB404-30>
2019-05-07 15:00:19 +0800

    011111

---
+++ demoProj/src/demoProj/student.java
@@ -0,0 +1,16 @@
+package demoProj;
+
+public class student {
+	public String name;
+	public String address;
+	
+	student(){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
