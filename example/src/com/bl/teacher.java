b72b376425337837c9366c90b868572c0fb3f548
XS170202103512 <273483629@qq.com>
2019-05-16 16:34:44 +0800

    2019-5-16

--- example/src/com/bl/teacher.java
+++ example/src/com/bl/teacher.java
@@ -4,7 +4,7 @@ public class teacher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("good");
+       System.out.println("good teacher");
 	}
 
 }
